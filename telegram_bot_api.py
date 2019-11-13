f577ecf466fe203724d97f7f03de721ae0138b9d
viktor.orkush <viktor.orkush@gmail.com>
2019-11-13 13:29:08 +0200

    bug fixed, send notification for all users bot

--- telegram_bot_api.py
+++ telegram_bot_api.py
@@ -1,3 +1,4 @@
+import pickle
 import telebot
 import config
 
@@ -5,8 +6,26 @@ import config
 bot = telebot.TeleBot(config.TOKEN)
 
 
+def save_chat_info(chat_id):
+    chats_id = get_all_chats_id()
+    if chat_id not in chats_id:
+        chats_id.append(chat_id)
+        with open('chat_info.pkl', 'wb+') as file:
+            pickle.dump(chats_id, file)
+
+
+def get_all_chats_id():
+    with open('chat_info.pkl', 'rb') as file:
+        try:
+            chats_id = pickle.load(file)
+        except EOFError:
+            chats_id = []
+    return list(chats_id)
+
+
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
+    save_chat_info(message.chat.id)
     bot.send_message(message.chat.id, "Этот бот служит для уведомления о завершении сделки, сюда будут приходить "
                                       "уведомления об успешно завершенных сделках с ее название и датой")
 
@@ -17,7 +36,9 @@ def send_text(message):
 
 
 def send_message(message):
-    bot.send_message(chat_id=config.CHAT_ID, text=message)
+    chats_id = get_all_chats_id()
+    for item in chats_id:
+        bot.send_message(chat_id=item, text=message)
 
 
 if __name__ == "__main__":
